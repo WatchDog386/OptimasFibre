b7bc80ec3e7f615fef8eec9629e8dce7d8e24c62
WatchDog386 <korrifantes36@gmail.com>
2025-09-02 16:29:00 +0300

    updated:sitemap.xml

--- generate-sitemap.cjs
+++ generate-sitemap.cjs
@@ -1,7 +1,7 @@
 const sitemapGenerator = require('sitemap-generator');
 
 // Use your actual Netlify URL
-const generator = sitemapGenerator('https://knoxvilletechnologies.com', {
+const generator = sitemapGenerator('https://optimas-fibre.netlify.app/', {
   stripQuerystring: true,
   filepath: './public/sitemap.xml',
   maxDepth: 0,
